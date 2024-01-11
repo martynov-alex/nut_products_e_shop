d6bb0fa5570aa31e54d273e1884cd69f083e791f
Alex Martynov <martynov.alex@gmail.com>
2024-01-11 12:10:36 +0600

    Refactor signOut method in AccountScreenController

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -2,20 +2,27 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nut_products_e_shop/src/features/authentication/data/fake_auth_repository.dart';
 
 class AccountScreenController extends StateNotifier<AsyncValue<void>> {
-  AccountScreenController(this.fakeAuthRepository)
+  AccountScreenController(this.authRepository)
       : super(const AsyncValue<void>.data(null));
-  final FakeAuthRepository fakeAuthRepository;
+  final FakeAuthRepository authRepository;
 
   Future<bool> signOut() async {
-    try {
-      state = const AsyncValue<void>.loading();
-      await fakeAuthRepository.signOut();
-      state = const AsyncValue<void>.data(null);
-      return true;
-    } on Exception catch (e, st) {
-      state = AsyncValue<void>.error(e, st);
-      return false;
-    }
+    // * this we can replace
+    //   try {
+    //     state = const AsyncValue<void>.loading();
+    //     await fakeAuthRepository.signOut();
+    //     state = const AsyncValue<void>.data(null);
+    //     return true;
+    //   } on Exception catch (e, st) {
+    //     state = AsyncValue<void>.error(e, st);
+    //     return false;
+    //   }
+    // }
+    //
+    // * with this
+    state = const AsyncValue<void>.loading();
+    state = await AsyncValue.guard<void>(authRepository.signOut);
+    return state.hasError == false;
   }
 }
 
