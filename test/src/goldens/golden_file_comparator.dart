6db223921928f803947ae438f62e9c5f5a877127
Alex Martynov <martynov.alex@gmail.com>
2024-01-15 10:12:53 +0600

    Add GoldenDiffComparator

---
+++ test/src/goldens/golden_file_comparator.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: omit_local_variable_types
+
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+const _kGoldenDiffTolerance = 0.05;
+
+class GoldenDiffComparator extends LocalFileComparator {
+  final double tolerance;
+
+  GoldenDiffComparator(
+    String testFile, {
+    this.tolerance = _kGoldenDiffTolerance,
+  }) : super(Uri.parse(testFile));
+
+  @override
+  Future<bool> compare(Uint8List imageBytes, Uri golden) async {
+    final ComparisonResult result = await GoldenFileComparator.compareLists(
+      imageBytes,
+      await getGoldenBytes(golden),
+    );
+
+    if (!result.passed && result.diffPercent > tolerance) {
+      final String error = await generateFailureOutput(result, golden, basedir);
+      throw FlutterError(error);
+    }
+    if (!result.passed) {
+      log('A tolerable difference of ${result.diffPercent * 100}% was found when comparing $golden.');
+    }
+    return result.passed || result.diffPercent <= tolerance;
+  }
+}
--- test/src/goldens/golden_products_list_test.dart
+++ test/src/goldens/golden_products_list_test.dart
@@ -1,10 +1,11 @@
-import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nut_products_e_shop/src/app.dart';
+import 'package:path/path.dart' as path;
 
 import '../robot.dart';
+import 'golden_file_comparator.dart';
 
 void main() {
   final sizeVariant = ValueVariant<Size>({
@@ -22,16 +23,23 @@ void main() {
       await r.golden.loadMaterialIconFont();
       await r.pumpMyApp();
       await r.golden.precacheImages();
-      await expectLater(
-        find.byType(MyApp),
-        matchesGoldenFile(
-            'images/products_list_${currentSize.width.toInt()}x${currentSize.height.toInt()}.png'),
+
+      final goldenFileKey =
+          'products_list_${currentSize.width.toInt()}x${currentSize.height.toInt()}.png';
+
+      goldenFileComparator = GoldenDiffComparator(
+        path.join(
+          (goldenFileComparator as LocalFileComparator).basedir.toString(),
+          'goldenFileKey',
+        ),
       );
+
+      await expectLater(find.byType(MyApp), matchesGoldenFile(goldenFileKey));
     },
     variant: sizeVariant,
     tags: ['golden', 'products_list'],
     // Skip this test until we can run it successfully on CI without this error:
     // Golden "products_list_300x600.png": Pixel test failed, 2.52% diff detected.
-    skip: Platform.isLinux,
+    skip: false,
   );
 }
