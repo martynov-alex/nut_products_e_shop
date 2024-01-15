d90fee2c8fd49923e19fd6706101f411a519dc59
Alex Martynov <martynov.alex@gmail.com>
2024-01-15 09:34:43 +0600

    Move golden images to the separate folder

--- test/src/goldens/golden_products_list_test.dart
+++ test/src/goldens/golden_products_list_test.dart
@@ -24,7 +24,7 @@ void main() {
       await expectLater(
         find.byType(MyApp),
         matchesGoldenFile(
-            'products_list_${currentSize.width.toInt()}x${currentSize.height.toInt()}.png'),
+            'images/products_list_${currentSize.width.toInt()}x${currentSize.height.toInt()}.png'),
       );
     },
     variant: sizeVariant,
