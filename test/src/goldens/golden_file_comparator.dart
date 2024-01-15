d1b38d1fa7308ed0b46a401cd9f879941d64ee38
Alex Martynov <martynov.alex@gmail.com>
2024-01-15 11:24:46 +0600

    Make print log message for GoldenDiffComparator

--- test/src/goldens/golden_file_comparator.dart
+++ test/src/goldens/golden_file_comparator.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: omit_local_variable_types
 
-import 'dart:developer';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -26,7 +24,10 @@ class GoldenDiffComparator extends LocalFileComparator {
       final String error = await generateFailureOutput(result, golden, basedir);
       throw FlutterError(error);
     }
-    log('A tolerable difference of ${result.diffPercent * 100}% was found when comparing $golden.');
+
+    // ignore: avoid_print
+    print(
+        'A tolerable difference of ${result.diffPercent * 100}% was found when comparing $golden. Current max golden test tolerance settings is ${tolerance * 100}%.');
     return result.passed || result.diffPercent <= tolerance;
   }
 }
