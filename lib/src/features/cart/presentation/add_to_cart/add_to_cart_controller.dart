21bdb1f34c2f66cf0b60b311a5749e4349053276
Alex Martynov <martynov.alex@gmail.com>
2024-01-16 11:55:29 +0600

    Make AddToCartController

---
+++ lib/src/features/cart/presentation/add_to_cart/add_to_cart_controller.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nut_products_e_shop/src/features/cart/domain/item.dart';
+import 'package:nut_products_e_shop/src/features/cart/service/cart_service.dart';
+import 'package:nut_products_e_shop/src/features/products/domain/product.dart';
+
+class AddToCartController extends StateNotifier<AsyncValue<int>> {
+  AddToCartController({required this.cartService}) : super(const AsyncData(1));
+
+  final CartService cartService;
+
+  void updateQuantity(int quantity) {
+    state = AsyncData(quantity);
+  }
+
+  Future<void> addItem(ProductID productID) async {
+    final item = Item(productId: productID, quantity: state.value!);
+    state = const AsyncLoading<int>().copyWithPrevious(state);
+    final value = await AsyncValue.guard<void>(() => cartService.addItem(item));
+    if (value.hasError) {
+      state = AsyncError(value.error!, value.stackTrace!);
+    } else {
+      state = const AsyncData(1);
+    }
+  }
+}
+
+final addToCartController =
+    StateNotifierProvider<AddToCartController, AsyncValue<int>>((ref) {
+  final cartService = ref.watch(cartServiceProvider);
+  return AddToCartController(cartService: cartService);
+});
