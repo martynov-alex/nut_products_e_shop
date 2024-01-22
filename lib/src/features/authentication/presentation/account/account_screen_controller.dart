5d84d47eb20a6adb618d3024ecc4480bf59adab6
Alex Martynov <martynov.alex@gmail.com>
2024-01-22 11:23:47 +0600

    AsyncNotifier for AccountScreenController

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,19 +1,60 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:nut_products_e_shop/src/features/authentication/data/fake_auth_repository.dart';
+import 'dart:async';
 
-class AccountScreenController extends StateNotifier<AsyncValue<void>> {
-  AccountScreenController({required this.authRepository})
-      : super(const AsyncData<void>(null));
-  final FakeAuthRepository authRepository;
+import 'package:nut_products_e_shop/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'account_screen_controller.g.dart';
+
+@riverpod
+class AccountScreenController extends _$AccountScreenController {
+  @override
+  FutureOr<void> build() async {
+    // * some async work
+    // await someFuture();
+    // * return comeOtherFuture()
+    // return ref.read(someProvider).someFuture();
+
+    // in our case we leave the body empty
+  }
 
   Future<void> signOut() async {
+    final authRepository = ref.read(authRepositoryProvider);
     state = const AsyncLoading<void>();
     state = await AsyncValue.guard<void>(authRepository.signOut);
   }
 }
 
-final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
-    AccountScreenController, AsyncValue<void>>((ref) {
-  final authRepository = ref.watch(authRepositoryProvider);
-  return AccountScreenController(authRepository: authRepository);
-});
+// * AsyncNotifier without Riverpod generator
+// class AccountScreenController extends AutoDisposeAsyncNotifier<void> {
+//   @override
+//   FutureOr<void> build() async {}
+
+//   Future<void> signOut() async {
+//     final authRepository = ref.read(authRepositoryProvider);
+//     state = const AsyncLoading<void>();
+//     state = await AsyncValue.guard<void>(authRepository.signOut);
+//   }
+// }
+
+// * Provider without Riverpod generator
+// final accountScreenControllerProvider =
+//     AutoDisposeAsyncNotifierProvider<AccountScreenController, void>(
+//         AccountScreenController.new);
+
+// * StateNotifier version
+// class AccountScreenController extends StateNotifier<AsyncValue<void>> {
+//   AccountScreenController({required this.authRepository})
+//       : super(const AsyncData<void>(null));
+//   final FakeAuthRepository authRepository;
+
+//   Future<void> signOut() async {
+//     state = const AsyncLoading<void>();
+//     state = await AsyncValue.guard<void>(authRepository.signOut);
+//   }
+// }
+
+// final accountScreenControllerProvider = StateNotifierProvider.autoDispose<
+//     AccountScreenController, AsyncValue<void>>((ref) {
+//   final authRepository = ref.watch(authRepositoryProvider);
+//   return AccountScreenController(authRepository: authRepository);
+// });
---
+++ lib/src/features/authentication/presentation/account/account_screen_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'account_screen_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$accountScreenControllerHash() =>
+    r'034e6c1b9ab0944642bf6ded85847e81f6d09933';
+
+/// See also [AccountScreenController].
+@ProviderFor(AccountScreenController)
+final accountScreenControllerProvider =
+    AutoDisposeAsyncNotifierProvider<AccountScreenController, void>.internal(
+  AccountScreenController.new,
+  name: r'accountScreenControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$accountScreenControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AccountScreenController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
